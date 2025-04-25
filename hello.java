7bef2c06368d62386fcfc960722aae7b36d3238e
musharafgithub <162470439+musharafgithub@users.noreply.github.com>
2025-04-25 10:44:12 +0530

    hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello{
-public static void main(string args[]){
-system.out.println("hello everyone");
+public static void main(String args[]){
+System.out.println("hello everyone");
 }
 }
